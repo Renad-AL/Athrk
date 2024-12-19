d244d492f44621cd4357b4071648f2c19879e603
Renad <uniirena@gmail.com>
2024-12-20 02:16:36 +0300

    Initial Commit

---
+++ Athrk/AthrkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthrkApp.swift
+//  Athrk
+//
+//  Created by Renad Alyousef on 19/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AthrkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AthrkTests/AthrkTests.swift
@@ -0,0 +1,17 @@
+//
+//  AthrkTests.swift
+//  AthrkTests
+//
+//  Created by Renad Alyousef on 19/06/1446 AH.
+//
+
+import Testing
+@testable import Athrk
+
+struct AthrkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
